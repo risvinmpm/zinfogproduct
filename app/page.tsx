42f6ba2856a178ca93dc78c680be255261baecca
risvinmpm <risvinmp39@gmail.com>
2025-06-27 17:15:59 +0530

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Hero from "@/components/layout/Hero";
 import Navbar from "@/components/layout/Navbar";
-import Aboutus from "../components/main/Aboutus";
 import Features from "@/components/main/Features";
 import Training from "../components/main/Training";
 import Team from "@/components/main/Team";
